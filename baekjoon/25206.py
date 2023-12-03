e95b254fe5664d33814936c2cfb599026aaa5363
inthyes <hyesoo0806@gmail.com>
2023-12-03 19:37:19 +0900

    baekjoon 25206

---
+++ baekjoon/25206.py
@@ -0,0 +1,10 @@
+R = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0}
+avg = 0
+sum = 0
+for i in range(20):
+    x, y, z = input().split()
+    if z != 'P':
+        avg += R[z] * float(y)
+        sum += float(y)
+
+print('%.6f' %(avg / sum))
