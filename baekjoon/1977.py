61d77dac2581ec348e08ba50f9e108d3aa0a5820
inthyes <hyesoo0806@gmail.com>
2023-12-16 19:30:28 +0900

    baekjoon 1977

---
+++ baekjoon/1977.py
@@ -0,0 +1,15 @@
+m = int(input())
+n = int(input())
+i = m
+li = []
+while i <= n:
+    if int(i ** 0.5) ** 2 == i:
+        li.append(i)
+    i += 1
+if not li:
+    print(-1)
+else:
+    print(sum(li))
+    print(min(li))
+
+
